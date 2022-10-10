1216d323e3549513d2532006963e145a3ecf8ba2
Elin Asplund <elin.asplund91@gmail.com>
2022-10-10 14:15:42 +0200

    added a form validation

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ function klick(){
 }
 
 
-
-
 // Funktion som kollar/validerar om input.value har en längd på sin sträng (dvs antal tecken) som är 0. Returnerar en boolean. 
 const isNullOrEmpty = value => {
 
@@ -21,6 +19,17 @@ const isNullOrEmpty = value => {
 // Funktion som kollar/validerar om input.value har en längd på sin sträng (dvs antal tecken) som är minst 2 tecken lång. Returnerar en boolean. 
 const isMinimumLength = (value, minLenght = 2) => {
     if (value.length >= minLenght)
+        return true
+
+    return false
+}
+
+// Funktion som kollar/validerar om input.value är på formatet x@x.xx. Returnerar en boolean. 
+const isEmailValid = email =>{
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // Regual expression för email på formatet x@x.xx
+
+    if (regEx.test(email))
         return true
 
     return false
@@ -51,9 +60,9 @@ const onSubmit = event => {
             let error=""
 
             switch(element.type){
+
                 case 'text':
                     if (!isNullOrEmpty(element.value)){
-
                         if (!isMinimumLength(element.value, 2)){
                             error = `Your name must contain at least 2 letters.`
                             // ${lable.toLowerCase()} 
@@ -68,10 +77,34 @@ const onSubmit = event => {
                     break;
 
                 case 'email':
-                    break;
+                    if (!isNullOrEmpty(element.value)){
+                        if (!isEmailValid(element.value)){
+                            error = `Your email must be valid (eg. example@domain.com)`
+                            // ${lable.toLowerCase()} 
+                            console.log(error);
+                        } else{
+                            console.log("Success!");
+                        }
+                    } else{
+                        error = `Please enter an valid email!`
+                        console.log(error);
+                    }
+                    break; 
 
                 case 'textarea':
-                    break;    
+                    if (!isNullOrEmpty(element.value)){
+                        if (!isMinimumLength(element.value, 20)){
+                            error = `Your comment must contain at least 20 characters.`
+                            // ${lable.toLowerCase()} 
+                            console.log(error);
+                        } else{
+                            console.log("Success!");
+                        }
+                    } else{
+                        error = `Please enter a comment!`
+                        console.log(error);
+                    }
+                    break;   
             }
 
             document.getElementById(`${element.id}-error`).innerText = error
