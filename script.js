e0b8ddd0bfd16c5fa53025c8018080a471de936b
Elin Asplund <elin.asplund91@gmail.com>
2022-10-11 14:15:26 +0200

    added comments in .js, for learning purposes

--- script.js
+++ script.js
@@ -52,32 +52,45 @@ const isEmailValid = email => {
 // onSubmit-event for form on contact.html page
 const onSubmitContact = event => {
     let error = ""
+    // Deklarerar och initierar error meddelandet till en tom sträng
     let success = true
+    // Boolean för att avgöra om valideringen är godkänd.
     event.preventDefault()
     // Hindrar att default beteendet utförs (inte försöker skicka iväg formulär-datan vid klick på submit-knapp?)
 
     for (let element of event.target) {
+    // for-loop som går genom event.target(=formuläret), letar efter elementen däri och sparar dem i den initierade variabeln element
         error = ""
+        // nollställer error för att slutreslutatet ska bli "rätt"
         if (element.required) {
+        // if-sats som kollar vilka element i formuläret som är required 
+
             let label = document.getElementById(`${element.id}-label`).innerText
-
+            // Deklarerar och initierar variabeln label som används vid utskrivning av error meddelandet
             switch (element.type) {
-
+            // switch som kollar på elementens typ ('text','email','textarea') och utför kod utifrån dessa typer. 
                 case 'text':
+                // vid element.type 'text' utförs:
                     if (!isNullOrEmpty(element.value)) {
+                    // if-sats: är input inte tom?
                         if (!isMinimumLength(element.value, 2)) {
+                        // if-sats: har inputen mindre än 2 tecken?
                             error = `Your ${label.toLocaleLowerCase()} must contain at least 2 letters.`
                             console.log(error);
                         } else {
+                        // om input har fler än 2 tecken: 
                             console.log("Success!");
                         }
                     } else {
+                    // om input är tom:
                         error = `Please enter a name!`
                         console.log(error);
                     }
                     break;
 
+                // följande case är av liknande sort som 'text' 
                 case 'email':
+                // vid element.type 'email' utförs:
                     if (!isNullOrEmpty(element.value)) {
                         if (!isEmailValid(element.value)) {
                             error = `Your ${label.toLocaleLowerCase()} must be valid (eg. example@domain.com).`
@@ -92,6 +105,7 @@ const onSubmitContact = event => {
                     break;
 
                 case 'textarea':
+                // vid element.type 'text' utförs:
                     if (!isNullOrEmpty(element.value)) {
                             // if (!isMinimumLength(element.value, 10)) {
                             //     error = `Your comment must contain at least 10 characters.`
@@ -109,16 +123,24 @@ const onSubmitContact = event => {
             }
 
             document.getElementById(`${element.id}-error`).innerText = error
+            // skriver ut error meddelandet i domen (i "html"-koden)
 
             if (error !== "") {
+            // if-sats som kollar om error meddelandet är en sträng med tecken => sätter success till false. 
+            // Om det är en tom sträng kommer ej gå in i if-satsen och success fortsätter att vara true som den initierades till i början av funktionen.
                 success = false
             }
         }
     }
 
     if (success) {
+    // if-sats som kollar om success=true
         document.getElementById(`successful-post`).innerHTML = '<div class="text-success">Comment posted!</div>'
-        // ToDo: nollställ alla inputfält!
+        // Postar ett bekräftade meddelande i domen att kommentaren är postad. 
+
+        // ToDo: 
+            // *Nollställ alla inputfält! 
+            // *Skicka data till server?
     }
 }
 
@@ -131,7 +153,7 @@ const onSubmitProduct = event => {
 const onClickHeadings = event => {
     event.preventDefault()
 }
-
+// EV KOD SOM VILL SPARA??
 //     let descriptionText= document.getElementById("description-Text")
 //     let additionalText= document.getElementById("additional-Text")
 
